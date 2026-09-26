eb3faf82f91d8ab2e1f5679bbde570325cde5466
atarovart <at.art7100@gmail.com>
2026-09-27 00:19:11 +0700

    site10.release

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          dark: '#0b0f19',
+          card: '#111827',
+          border: '#1f2937',
+          cyan: '#06b6d4',
+          emerald: '#10b981',
+          blue: '#3b82f6'
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+};
